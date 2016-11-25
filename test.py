cf98bcee6588c8b195795a8af15324aacfa566bc
qinabu <qinabu@gmail.com>
2016-11-25 23:05:28 +0300

    test

---
+++ test.py
@@ -0,0 +1,53 @@
+import collections
+import re
+
+Token = collections.namedtuple('Token', ['type', 'value', 'line', 'column'])
+
+
+def tokenize(code):
+    token_specification = [
+        ('WORD', r'\w+'),
+        ('OTHER', r'\W+'),
+        ('NEWLINE', r'\n'),
+    ]
+    tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+    line_num = 1
+    line_start = 0
+    for mo in re.finditer(tok_regex, code):
+        kind = mo.lastgroup
+        value = mo.group(kind)
+        if kind == 'NEWLINE':
+            line_start = mo.end()
+            line_num += 1
+            yield Token(kind, value, line_num, 0)
+        else:
+            column = mo.start() - line_start
+            yield Token(kind, value, line_num, column)
+
+
+
+words = {}
+
+def gen(word):
+    if word in words:
+        return words[word]
+    # todo generate
+    new_word = 'allal'
+    words[word] = new_word
+    return new_word
+
+statements = '''
+A, fairy tale is a type of short story that typically features folkloric fantasy characters, such as dwarves, elves,
+fairies, giants, gnomes, goblins, mermaids, trolls, unicorns, or witches, and usually magic or enchantments.
+Fairy tales may be distinguished from other folk narratives such as legends (which generally involve belief in the
+veracity of the events described)[1] and explicitly moral tales, including beast fables. The term is mainly used for
+stories with origins in European tradition and, at least in recent centuries, mostly relates to children's literature.
+'''
+
+
+for token in tokenize(statements):
+    if token.type == 'WORD':
+        print(gen(token.value), end='')
+    else:
+        print(token.value, end='')
+
