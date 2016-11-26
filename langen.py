5913bd5312e6da86ebcb5cc52696331a10b66279
Dmitry Khasanov <dkhasanov@avito.ru>
2016-11-26 02:40:59 +0200

    Gitignore improved.

--- langen.py
+++ langen.py
@@ -5,12 +5,8 @@ from lib.generator import Generator
 
 def main():
     gen = Generator()
-    i = 0
-    for s in gen.get_syllables(allow_vowel_start=False):
-        print(s)
-        i = i + 1
-        if i == 1000:
-            break
+    gen.set_seed("someseed")
+    print(gen.gen_root())
 
 
 if __name__ == '__main__':
--- lib/generator.py
+++ lib/generator.py
@@ -1,15 +1,17 @@
+import sys
 import random
-import itertools
 
 
 class Generator:
-    def gen_root(self, seed):
+    def set_seed(self, seed):
         random.seed(seed)
+
+    def gen_root(self):
         min_syllables = 2
         max_syllables = 4
         syllables_count = random.randrange(
-                min_syllables - 1,
-                max_syllables - 1
+                min_syllables,
+                max_syllables
                 )
         syllables = self.get_syllables()
         word = []
@@ -20,23 +22,10 @@ class Generator:
     def get_syllables(self,
                       vowels='euioa',
                       consonants='rtpsdfghklzvbnm',
-                      min_len=2,
-                      max_len=3,
-                      allow_vowel_start=True,
-                      allow_vowel_end=True,
-                      allow_consecutive_vowels=True,
-                      allow_similar_consecutive_vowels=True
                       ):
-        result = itertools.combinations_with_replacement(
-                vowels + consonants,
-                max_len
-                )
-        for r in result:
-            if not allow_vowel_start and vowels.find(r[0]) > -1:
-                continue
-            if not allow_vowel_end and vowels.find(r[:-1]) > -1:
-                continue
-            # filter results with consecutive consonants
-            # TODO filter consecutive vowels
-            # TODO filter similar consecutive vowels
-            yield ''.join(r)
+        result = []
+        for v in vowels:
+            for c in consonants:
+                result.append('%s%s' % (v, c))
+                result.append('%s%s' % (c, v))
+        return result
