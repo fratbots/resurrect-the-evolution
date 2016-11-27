ee05f270145dace6cf2dc9ffb9166c0036c5a692
qinabu <qinabu@gmail.com>
2016-11-27 11:09:23 +0300

    web~

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from lib.lang import *
+import random
 
 app = Flask(__name__)
 
@@ -7,7 +9,17 @@ def gen():
     lang_name = request.form.get('lang_name', '')
     input_text = request.form.get('input_text', '')
     if request.method == 'POST':
-        pass
+
+        random.seed(lang_name)
+        language = Language(generator)
+
+        translated = language.translate(input_text)
+        print_grammar(language)
+        print_dictionary(language)
+
+        return translated
+
+
     return render_template('gen.html',
             lang_name=lang_name,
             input_text=input_text
